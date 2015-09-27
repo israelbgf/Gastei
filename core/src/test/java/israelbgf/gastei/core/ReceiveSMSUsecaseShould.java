dabf809c34fefc9eb07b293d9737fe37553c4af8
Israel Fonseca <israel.bgf@gmail.com>
2015-09-27 11:37:09 -0300

    Test refactoring

--- core/src/test/java/israelbgf/gastei/core/ReceiveSMSUsecaseShould.java
+++ core/src/test/java/israelbgf/gastei/core/ReceiveSMSUsecaseShould.java
@@ -3,6 +3,7 @@ package israelbgf.gastei.core;
 import israelbgf.gastei.core.entities.Expense;
 import israelbgf.gastei.core.gateways.ExpenseGateway;
 import israelbgf.gastei.core.usecases.ReceiveSMSUsecase;
+import org.junit.After;
 import org.junit.Test;
 
 import static israelbgf.gastei.core.utils.DateUtils.createDate;
@@ -15,12 +16,12 @@ public class ReceiveSMSUsecaseShould {
             "26/09/2015 13:46. VALOR DE $ 156,47 NO(A) GIASSI SUPERMERCADOS LOJAJOINVILLE";
     public static final String INVALID_SMS_CONTENT = "MENSAGEM ERRADA";
 
+    ExpenseGateway gateway = mock(ExpenseGateway.class);
+    ReceiveSMSUsecase.Presenter presenter = mock(ReceiveSMSUsecase.Presenter.class);
+    ReceiveSMSUsecase usecase = new ReceiveSMSUsecase(gateway, presenter);
+
     @Test
     public void storeExpenseWhenParseValidSMS() {
-        ExpenseGateway gateway = mock(ExpenseGateway.class);
-        ReceiveSMSUsecase.Presenter presenter = mock(ReceiveSMSUsecase.Presenter.class);
-        ReceiveSMSUsecase usecase = new ReceiveSMSUsecase(gateway, presenter);
-
         usecase.execute(VALID_NUMBER, SMS_CONTENT);
 
         Expense expectedExpense = new Expense(
@@ -30,18 +31,17 @@ public class ReceiveSMSUsecaseShould {
 
         verify(presenter).presentNewExpenseAdded(expectedExpense);
         verify(gateway).save(expectedExpense);
-        verifyNoMoreInteractions(gateway, presenter);
     }
 
     @Test
     public void notifyErrorWhenParseInvalidSMS() {
-        ExpenseGateway gateway = mock(ExpenseGateway.class);
-        ReceiveSMSUsecase.Presenter presenter = mock(ReceiveSMSUsecase.Presenter.class);
-        ReceiveSMSUsecase usecase = new ReceiveSMSUsecase(gateway, presenter);
-
         usecase.execute(VALID_NUMBER, INVALID_SMS_CONTENT);
 
         verify(presenter).presentInvalidSMSContent(INVALID_SMS_CONTENT);
+    }
+
+    @After
+    public void after(){
         verifyNoMoreInteractions(gateway, presenter);
     }
 
