f5de4dccbb59b7a7422b8f829b153415c1d2b126
Israel Fonseca <israel.bgf@gmail.com>
2015-10-19 21:54:52 -0200

    Better presentation of the expense

--- mobile/src/main/java/israelbgf/gastei/mobile/presenters/ListMonthlyExpensesUsecasePresenter.java
+++ mobile/src/main/java/israelbgf/gastei/mobile/presenters/ListMonthlyExpensesUsecasePresenter.java
@@ -1,38 +1,64 @@
 package israelbgf.gastei.mobile.presenters;
 
 import android.app.ListActivity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 import israelbgf.gastei.core.entities.ExpenseEntity;
 import israelbgf.gastei.core.usecases.ListMonthlyExpensesUsecase.Presenter;
+import israelbgf.gastei.mobile.R;
 
-import java.text.SimpleDateFormat;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 public class ListMonthlyExpensesUsecasePresenter implements Presenter {
 
-    private List<String> itens = new ArrayList<>();
-    private ArrayAdapter<String> adapter;
+    DecimalFormat CURRENCY_FORMATTER = new DecimalFormat("$#.##");
+
+    private List<ExpenseEntity> itens = new ArrayList<>();
+    private ExpenseAdapter adapter;
     private ListActivity activity;
 
     public ListMonthlyExpensesUsecasePresenter(ListActivity activity) {
         this.activity = activity;
-        this.adapter = new ArrayAdapter<>(activity, android.R.layout.simple_list_item_1, itens);
+        this.adapter = new ExpenseAdapter(activity, itens);
         this.activity.setListAdapter(adapter);
     }
 
     @Override
     public void presentExpenses(Struct struct) {
         itens.clear();
-        for (ExpenseEntity expense : struct.expenses) {
-            itens.add(formatted(expense));
-        }
+        itens.addAll(struct.expenses);
         adapter.notifyDataSetChanged();
     }
 
-    private String formatted(ExpenseEntity expense) {
-        String formattedDate = new SimpleDateFormat("yyyy/MM/dd", Locale.getDefault()).format(expense.getDate());
-        return String.format("%s - %s R$%s", formattedDate, expense.getPlace(), expense.getAmount());
+    private class ExpenseAdapter extends ArrayAdapter<ExpenseEntity> {
+
+        public ExpenseAdapter(Context context, List<ExpenseEntity> itens) {
+            super(context, -1, itens);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            if (convertView == null)
+                convertView = inflater.inflate(R.layout.expense_item, parent, false);
+
+            ExpenseEntity expense = getItem(position);
+            TextView amountView = (TextView) convertView.findViewById(R.id.amount);
+            TextView placeView = (TextView) convertView.findViewById(R.id.place);
+            TextView cityView = (TextView) convertView.findViewById(R.id.city);
+
+            amountView.setText(CURRENCY_FORMATTER.format(expense.getAmount()));
+            placeView.setText(expense.getPlace());
+            cityView.setText("CITY");
+
+            return convertView;
+        }
     }
 }
